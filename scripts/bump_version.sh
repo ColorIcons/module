096b45f4297e050e707a63f49f8add9e758b0cba
immortal521 <immort521@gmail.com>
2026-04-02 23:15:53 +0800

    ci: add GitHub Actions workflow for build and release
    - Add bump_version job to auto-increment version
    
    - Build Rust for Android arm64
    
    - Build and package WebUI
    
    - Create GitHub release with module zip
    
    - Add bump_version.sh script for version management

---
+++ scripts/bump_version.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+# 获取上一个 commit message 决定 bump 类型
+LAST_MSG=$(git log -1 --pretty=%B)
+
+if [[ "$LAST_MSG" =~ BREAKING ]]; then
+  TYPE="major"
+elif [[ "$LAST_MSG" =~ feat ]]; then
+  TYPE="minor"
+else
+  TYPE="patch"
+fi
+
+# Rust Cargo.toml
+CURRENT_VERSION=$(grep '^version =' Cargo.toml | head -n1 | cut -d '"' -f2)
+IFS='.' read -r MAJOR MINOR PATCH <<<"$CURRENT_VERSION"
+
+case $TYPE in
+major)
+  MAJOR=$((MAJOR + 1))
+  MINOR=0
+  PATCH=0
+  ;;
+minor)
+  MINOR=$((MINOR + 1))
+  PATCH=0
+  ;;
+patch)
+  PATCH=$((PATCH + 1))
+  ;;
+esac
+
+NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
+NEW_VERSIONCODE=$(printf "%03d%02d%01d" "$MAJOR" "$MINOR" "$PATCH")
+
+# 更新 Cargo.toml
+sed -i "s/^version = \".*\"/version = \"$NEW_VERSION\"/" Cargo.toml
+
+# 更新 WebUI package.json（子模块）
+jq ".version=\"$NEW_VERSION\"" webui/package.json >webui/package.json.tmp && mv webui/package.json.tmp webui/package.json
+
+# 更新 module/module.prop
+sed -i "s/^version=.*/version=$NEW_VERSION/" module/module.prop
+sed -i "s/^versionCode=.*/versionCode=$NEW_VERSIONCODE/" module/module.prop
+
+# 添加所有改动到 git，推送由 action 做
+git add Cargo.toml webui/package.json module/module.prop
+
+echo "Bumped version to $NEW_VERSION (versionCode=$NEW_VERSIONCODE)"
